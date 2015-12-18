4c545405dc3e7496e066f91b7a99b991e1f1cdc2
sanchezzzhak <sanchezzzhak@ya.ru>
2015-12-18 15:58:22 +0300

    add header footer and slim scroll jquery

--- Panel.php
+++ Panel.php
@@ -16,7 +16,7 @@ class Panel extends Widget
      * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
      */
     public $options = [];
-
+    public $bodyOptions = [];
 
    /* public $layout;
     public $buttons;*/
@@ -24,9 +24,15 @@ class Panel extends Widget
 
     /** @var  string the panel-title */
     public $title;
-    public $height;
     public $heading = true;
+
     public $content;
+    public $header;
+    public $footer;
+
+    public $slimScroll = false;
+    public $slimOptions = [];
+
 
     public function init()
     {
@@ -35,33 +41,49 @@ class Panel extends Widget
         echo Html::beginTag('div',$this->options);
 
         if($this->heading)
-            echo Html::tag('div', Html::tag('h4',$this->title), ['class' => 'panel-heading']);
+            echo Html::tag('div', Html::tag('h4',$this->title) . $this->header , ['class' => 'panel-heading']);
 
-        echo Html::beginTag('div',['class' => 'panel-body']);
-            echo $this->content;
+        Html::addCssClass($this->bodyOptions,'panel-body');
+        echo Html::beginTag('div',$this->bodyOptions);
+        echo $this->content;
+    }
+
+    protected function prepareOptionsToAttr($attrName,$options= [])
+    {
+        $strArr = [];
+        foreach($options as $key => $option) {
+            $strArr[$attrName . '-'.$key] =  $option;
+        }
+        return $strArr;
     }
 
     public function run()
     {
         echo Html::endTag('div');
+
+        if(!empty($this->footer)){
+            echo  Html::tag('div', $this->footer , ['class' => 'panel-footer']);
+        }
         echo Html::endTag('div');
     }
 
 
     private function initOptions()
     {
+        $view = $this->getView();
+        if($this->slimScroll) {
+            $this->bodyOptions = ArrayHelper::merge($this->bodyOptions, $this->prepareOptionsToAttr('data-slim',$this->slimOptions));
+            Html::addCssClass($this->bodyOptions,'slimScrollPanel');
+            SlimScrollAsset::register($view);
+        }
         if (!isset($this->options['id'])) {
             $this->options['id'] = $this->getId();
         }
-        if (!isset($this->options['class'])) {
-            $this->options['class'] = ' panel panel-inverse';
-        }
-        $view = $this->getView();
-        if($this->options['data-height'] = $this->height ) {
-            $this->options['class'] .=' slimScrollPanel';
-            SlimScrollAsset::register($view);
-        }
 
+        if (!isset($this->options['class'])) {
+            $this->options['class'] = 'panel-inverse';
+        }
+        Html::addCssClass($this->options,'panel kak-panel');
         PanelAsset::register($view);
     }
 
--- assets/kak-panel.js
+++ assets/kak-panel.js
@@ -1,3 +1,55 @@
+(function(root, factory) {
+    // CommonJS support
+    if (typeof exports === 'object') {
+        module.exports = factory();
+    }
+    // AMD
+    else if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    }
+    // Browser globals
+    else {
+        factory(root.jQuery);
+    }
+}(this, function($) {
+    'use strict';
+
+    // **********************************
+    // Constructor
+    // **********************************
+
+    var isSlimPanel = typeof $.fn.slimScroll !== 'undefined';
+
+    var kakPanel = function(element, options) {
+        this.$parent = $(element)
+
+        if (isSlimPanel){
+            this.$parent.find('.slimScrollPanel').slimScroll({
+               // height: $(item).data('height') + 'px'
+            });
+        }
+    };
+
+    kakPanel.prototype = {
+        constructor: kakPanel
+    };
+
+    $.fn.kakPanel = function(option) {
+        var options = typeof option == 'object' && option;
+        new kakPanel(this, options);
+        return this;
+    };
+    $.fn.kakPanel.Constructor = kakPanel;
+
+    // auto init
+    $('.kak-panel').each(function(k,i){
+        $(i).kakPanel();
+    });
+
+}));
+
+
+/*
 (function($){
     if(typeof $.fn.slimScroll !== 'undefined') {
         $.each($('.slimScrollPanel'), function(k,item) {
@@ -7,4 +59,4 @@
 
         });
     }
-})(jQuery);
+})(jQuery);*/
